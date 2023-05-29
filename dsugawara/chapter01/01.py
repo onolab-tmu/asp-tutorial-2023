063535667afd0a7a0481d2aed81df4059d9cd005
dsugawara <dksuga3652@gmail.com>
2023-05-29 19:05:32 +0900

    不足部分の追加

--- dsugawara/chapter01/01.py
+++ dsugawara/chapter01/01.py
@@ -1,6 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def make_sine(A, f, sec, fs):
+    """正弦波を生成
+    Args:
+        A(int):振幅
+        f(int):周波数
+        sec(int):信号長
+        fs(int):サンプリング周波数
+    Return:
+        x(ndarray):正弦波
+        t(ndarray):時間配列
+    """
+    t = np.arange(0, sec, 1 / fs)
+    # 正弦波
+    x = A * np.sin(2 * np.pi * f * t)
+
+    return x, t
+
+
 if __name__ == "__main__":
-    A=1
-    
+
+    x, t = make_sine(A=1, f=440, sec=3, fs=16000)
+
+    plt.figure(figsize=[6.0, 4.0])
+    plt.title("440Hz")
+    plt.plot(t, x)
+    plt.xlim(0, 0.03)
+    plt.savefig("01.pdf")
