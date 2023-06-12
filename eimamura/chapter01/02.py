39dc10863aae5cc47f5b02170cd9d9bb72b2cf9e
Imamura Eigetsu <eigetsu@ImamuranoMacBook-Pro.local>
2023-06-12 09:25:24 +0900

    02 add

--- eimamura/chapter01/02.py
+++ eimamura/chapter01/02.py
@@ -1,6 +1,14 @@
 import numpy as np
 import soundfile as sf
 
+a = 1
+f = 440
+fs = 16000
+s = 3
+
+t = np.arange(0, s, 1/fs)
+y = a * np.sin(2*np.pi*f*t)
+
 sf.write('1_2.wav', y, fs)
 
 sf.info('1_2.wav')
