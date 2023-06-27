d6f2dfe941ee14792ab7fefc07032b9b4c5b581f
rkatotmu <rkatotmu@gmail.com>
2023-06-27 11:17:54 +0900

    3章後半

---
+++ rkato/chapter03/q07.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# 再帰関数
+def recursive_func(x, a, b, n):
+    if n == 0:
+        return b[n] / a[n] * x[n]
+    else:
+        y = -a[n] * recursive_func(x, a, b, n - 1) + b[n] * x[n]
+        return y / a[0]
+
+
+# 差分方程式（一般系）
+def difference_equation_recursive(x, a, b):
+    N = len(x)
+    y = np.zeros(N)
+
+    for n in range(N):
+        y[n] = recursive_func(x, a, b, n)
+    return y
+
+
+# 確認
+x = np.zeros(10)
+x[0] = 1
+a = np.zeros(10) - 3
+a[0] = 10
+b = np.zeros(10) + 4
+y = difference_equation_recursive(x, a, b)
+np.set_printoptions(suppress=True)
+print(y)
+# [0.4   0.12   0.036   0.0108    0.00324   0.000972   0.0002916  0.00008748 0.00002624 0.00000787]
+
+plt.stem(y)
+plt.show()
---
+++ rkato/chapter03/q08.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# 周波数応答
+def frequency_responce(a, b, omega):
+    a_sum = np.sum(a[1:] * np.exp(-1j * omega * np.arange(1, len(a))))
+    b_sum = np.sum(b * np.exp(-1j * omega * np.arange(len(b))))
+
+    H = b_sum / (1 + a_sum)
+
+    return H
+
+
+# 確認コード
+N = 10
+a = np.zeros(N)
+a[0] = 1
+b = np.zeros(N)
+b[0] = 1
+fs = 16000
+f = np.arange(N * fs) / N
+omega = 2 * np.pi * f / fs
+H = np.zeros(N, dtype=complex)
+for i in range(N):
+    H[i] = frequency_responce(a, b, omega[i])
+
+plt.subplot(2, 1, 1)
+plt.plot(np.abs(H))
+plt.subplot(2, 1, 2)
+plt.plot(np.angle(H))
+plt.show()
