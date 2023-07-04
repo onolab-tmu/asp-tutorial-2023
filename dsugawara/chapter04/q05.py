7413a6157bcc5d620895a0bfadd0fed50f49adc4
dsugawara <dksuga3652@gmail.com>
2023-07-04 12:40:46 +0900

    q05.pyにdocstringを追加

--- dsugawara/chapter04/q05.py
+++ dsugawara/chapter04/q05.py
@@ -1,7 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def optimal_win(S, win):
+    """合成窓を作成
+    Args:
+        S(int): シフト幅
+        win(ndarray): 窓
+    Return:
+        win_s: 最適合成窓
+    """
     L = win.size
     Q = L // S
     for l in range(0, L):
