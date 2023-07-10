c18665e98df3790d7138ddc9771f27e70beb0d44
dsugawara <dksuga3652@gmail.com>
2023-07-11 06:37:36 +0900

    シグマ計算をする範囲を修正

--- dsugawara/chapter04/q05.py
+++ dsugawara/chapter04/q05.py
@@ -8,14 +8,17 @@ def optimal_win(S, win):
         S(int): シフト幅
         win(ndarray): 窓
     Return:
-        win_s: 最適合成窓
+        win_opt: 最適合成窓
     """
+
     L = win.size
     Q = L // S
+    win_opt = np.empty(L)
     for l in range(0, L):
         sigma = 0
-        for m in range(0, Q):
-            sigma += win[l - m * S] ** 2
-    win_s = win / sigma
+        for m in range(-(Q - 1), Q):
+            if l - m * S >= 0 and l - m * S < L:
+                sigma += win[l - m * S] ** 2
+        win_opt[l] = win[l] / sigma
 
-    return win_s
+    return win_opt
